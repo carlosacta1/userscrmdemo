eb94f5f2a183d2f5cbc6075f05d21f8ff2a5b9d6
Carlos Acosta <carlos428.ca@gmail.com>
2024-03-31 18:59:58 -0500

    Testing a MVC architecture

--- src/main/java/com/crmdemo/demo/controllers/UsersController.java
+++ src/main/java/com/crmdemo/demo/controllers/UsersController.java
@@ -3,21 +3,31 @@ package com.crmdemo.demo.controllers;
 import com.crmdemo.demo.dao.UserDao;
 import com.crmdemo.demo.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UsersController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping(value = "users")
-    public List<User> index(){
-        return userDao.getUsers();
+    @GetMapping(value = "users")
+    public String getUsers(Model model) {
+        List<User> users = userDao.getUsers();
+        model.addAttribute("users", users);
+        System.out.println("Este es un mensaje de logging.");
+        System.out.println(users);
+        return "users/index";
     }
+
     @RequestMapping(value = "user/{id}")
     public User show(@PathVariable Long id){
         User user = new User();
