b1ccb79580e5919c70699228d8834fc86b9430e0
Carlos Acosta <carlos428.ca@gmail.com>
2024-03-31 15:03:40 -0500

    Adding postgresql config

--- src/main/java/com/crmdemo/demo/controllers/UsersController.java
+++ src/main/java/com/crmdemo/demo/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.crmdemo.demo.controllers;
 
+import com.crmdemo.demo.dao.UserDao;
 import com.crmdemo.demo.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,25 +11,21 @@ import java.util.List;
 
 @RestController
 public class UsersController {
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value = "users")
     public List<User> index(){
-        User user = new User();
-        user.setFirstName("Carlos");
-        user.setLastName("Acosta");
-        user.setEmail("carlostest@gmail.com");
-        user.setPhone("123456");
-
-        return List.of(user);
+        return userDao.getUsers();
     }
     @RequestMapping(value = "user/{id}")
     public User show(@PathVariable Long id){
         User user = new User();
-        user.setId(id);
-        user.setFirstName("Carlos");
-        user.setLastName("Acosta");
-        user.setEmail("carlostest@gmail.com");
-        user.setPhone("123456");
+//        user.setId(id);
+//        user.setFirstName("Carlos");
+//        user.setLastName("Acosta");
+//        user.setEmail("carlostest@gmail.com");
+//        user.setPhone("123456");
 
         return user;
     }
@@ -35,10 +33,10 @@ public class UsersController {
     @RequestMapping(value = "user/edit")
     public User edit(){
         User user = new User();
-        user.setFirstName("Carlos");
-        user.setLastName("Acosta");
-        user.setEmail("carlostest@gmail.com");
-        user.setPhone("123456");
+//        user.setFirstName("Carlos");
+//        user.setLastName("Acosta");
+//        user.setEmail("carlostest@gmail.com");
+//        user.setPhone("123456");
 
         return user;
     }
@@ -46,10 +44,10 @@ public class UsersController {
     @RequestMapping(value = "user/delete")
     public User delete(){
         User user = new User();
-        user.setFirstName("Carlos");
-        user.setLastName("Acosta");
-        user.setEmail("carlostest@gmail.com");
-        user.setPhone("123456");
+//        user.setFirstName("Carlos");
+//        user.setLastName("Acosta");
+//        user.setEmail("carlostest@gmail.com");
+//        user.setPhone("123456");
 
         return user;
     }
